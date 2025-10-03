508ec187fccaca57e24418fccdbc16a98ddda361
A713\dam1 <kevinslayer186@gmail.com>
2025-10-03 16:00:43 +0200

    no message

---
+++ programacion2/src/main/java/org/example/Practica2_4.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Practica2_4 {
+    static void main() {
+        int numero1;
+        int numero2;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("introduce el primer numero (3 cifras)");
+        numero1 = teclado.nextInt();
+        System.out.println("introduce el primer numero (3 cifras)");
+        numero2 = teclado.nextInt();
+
+
+
+
+
+
+
+
+    }
+}
