2f01eb5bb6acff3d9b57a4c08e3e00a6a63c283b
A713\dam1 <kevinslayer186@gmail.com>
2025-11-13 19:27:51 +0100

    no message

---
+++ programacion2/src/main/java/org/example/eliminar_duplicados.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class eliminar_duplicados {
+    static void main() {
+        Integer vector[] = {1,2,2,2,4,6,6,6,6};
+        for (int i = 0;i<vector.length-1;i++){
+            for (int j = i+1; j<vector.length;i++){
+                if (vector[i]==vector[j]){
+
+                }
+            }
+        }
+    }
+}
