ec0ad7971354c2e1211703f4ca4ca59f4e9ed056
A713\dam1 <kevinslayer186@gmail.com>
2025-11-18 15:32:00 +0100

    no message

---
+++ programacion2/src/main/java/org/example/Primitiva.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Primitiva {
+    static void main() {
+        Random aleatorio = new Random();
+        Scanner teclado = new Scanner(System.in);
+        int numeroelegido[] = new int[7];
+        boolean control = true;
+        System.out.println("elige tu boleto con el siguiente formato N-N-N-N-N-N/R");
+        String Eleccion = teclado.next();
+
+    }
+}
